e308603710a91e050f9ee664a19d2252662c1b0f
xiaoyu <xiaohua@xy.com>
2026-04-23 18:17:02 +0800

    Alchemy

--- solanakit/src/main/java/io/horizontalsystems/solanakit/models/RpcSource.kt
+++ solanakit/src/main/java/io/horizontalsystems/solanakit/models/RpcSource.kt
@@ -9,5 +9,6 @@ sealed class RpcSource(var name: String, var endpoint: RPCEndpoint, val syncInte
 
     object Serum: RpcSource("Serum Project API", RPCEndpoint.mainnetBetaSerum, 30)
     object TritonOne: RpcSource("TritonOne API", RPCEndpoint.mainnetBetaSolana, 30)
+    object Alchemy: RpcSource("Alchemy API", RPCEndpoint.custom(URL("https://solana-mainnet.g.alchemy.com/v2/Hy5Pc7K9_36cmVlzm_yZDUrZqTYD3xMM"), URL("https://solana-mainnet.g.alchemy.com/v2/Hy5Pc7K9_36cmVlzm_yZDUrZqTYD3xMM"), Network.mainnetBeta), 30)
     class Custom(name: String, httpURL: URL, websocketURL: URL, syncInterval: Long): RpcSource(name, RPCEndpoint.custom(httpURL, websocketURL, Network.mainnetBeta), syncInterval)
 }
